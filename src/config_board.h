04d983ddb520124401184c9435394763b8b48539
nhhiep92 <nhhiep@fossil.com>
2022-05-06 22:33:00 +0700

    - increase chunk size - turn off debug log

--- src/config_board.h
+++ src/config_board.h
@@ -3,7 +3,7 @@
 #include <hardware/i2c.h>
 #include <hardware/pio.h>
 
-// #define CONFIG_NDEBUG
+#define CONFIG_NDEBUG
 
 #define CONFIG_PIN_CAM_RESETB (2)
 #define CONFIG_PIN_CAM_XCLK (3)
@@ -18,4 +18,4 @@
 #define USB_PORT (0)
 #define BUFFER_MAX_SIZE (256)
 #define IMAGE_SIZE (352 * 288 * 2)
-#define CHUNK_SIZE (200)
+#define CHUNK_SIZE (BUFFER_MAX_SIZE)
